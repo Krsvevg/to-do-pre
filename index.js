98ffae9d9cbd604f1ac66d2978aeaf5601aef796
krsvevg <krsvevg@gmail.com>
2025-12-16 22:10:09 +0500

    first

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+	if (localStorage.getItem('tasks')) {
+		return JSON.parse(localStorage.getItem('tasks'));
+	} else {
+	return items;
+	}
 }
 
 function createItem(item) {
@@ -23,13 +27,64 @@ function createItem(item) {
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
+	textElement.textContent = item;
+
+	deleteButton.addEventListener('click', function() {
+		clone.remove();
+		items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	duplicateButton.addEventListener('click', function() {
+		const itemName = textElement.textContent;
+		const newItem = createItem(item);
+		listElement.prepend(newItem);
+		const items = getTasksFromDOM();
+    saveTasks(items);
+	});
+
+	editButton.addEventListener('click', function() {
+		textElement.setAttribute('contenteditable', 'true');
+    textElement.focus();
+	});
+
+	 textElement.addEventListener('blur', function() {
+        textElement.setAttribute('contenteditable', 'false');
+        const items = getTasksFromDOM();
+        saveTasks(items);
+    });
+
+	return clone;
+
 }
 
 function getTasksFromDOM() {
-
+	var itemsNamesElements = listElement.querySelectorAll('.to-do__item-text');
+	tasks = [];
+	itemsNamesElements.forEach (function(element) {
+		tasks.push(element.textContent);
+	});
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+	localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+formElement.addEventListener('submit', function(ev) {
+	ev.preventDefault();
+	const taskText = inputElement.value.trim();
+	if (taskText) {
+		const newItem = createItem(taskText);
+		listElement.prepend(newItem);
+		const items = getTasksFromDOM();
+		saveTasks(items);
+		inputElement.value = '';
+	}
+})
+
+items = loadTasks();
+items.forEach(function(item){
+	const Element = createItem(item);
+	listElement.append(Element);
+});
